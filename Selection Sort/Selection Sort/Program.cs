bba419387769473ec9d7802251ba56dd52259ffd
Ülvi <114242983+UlviParvizoghlu@users.noreply.github.com>
2023-03-02 21:24:09 +0000

    Update Program.cs

--- Selection Sort/Selection Sort/Program.cs
+++ Selection Sort/Selection Sort/Program.cs
@@ -1,22 +1,21 @@
-﻿int[] arr = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+int[] arr = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
 int n = 10;
-int k, min;
-for (int i = 0; i < n - 1; i++)
+int temp, min;
+for (int i = 0; i < n-1; i++)
 {
     min = i;
-    for (int j = i + 1; j < n; j++)
+    for(int j=i+1;j<n;j++)
     {
         if (arr[j] < arr[min])
         {
             min = j;
         }
     }
-    k = arr[min];
+    temp = arr[min];
     arr[min] = arr[i];
-    arr[i] = k;
+    arr[i] = temp;
 }
-Console.Write("Sorted array is: ");
-for (int i = 0; i < n; i++)
+for (int i = 0;i < n;i++)
 {
-    Console.Write(arr[i] + " ");
+    Console.WriteLine(arr[i]);
 }
