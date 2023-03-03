367339730f34672905864ded544dc6fb11a2b48d
Ulvi Spaethe <ulvispaethe@gmail.com>
2023-03-03 01:23:28 +0000

    first commit

---
+++ Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+int n = 10;
+int k, min;
+for (int i = 0; i < n - 1; i++)
+{
+    min = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (arr[j] < arr[min])
+        {
+            min = j;
+        }
+    }
+    k = arr[min];
+    arr[min] = arr[i];
+    arr[i] = k;
+}
+Console.Write("Sorted array is: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
